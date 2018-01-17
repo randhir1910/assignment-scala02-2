e0352246c31d8692c1e3fdd0b276485537803f11
randhir <randhiricadbg@gmail.com>
2018-01-17 14:41:47 +0530

    completed..

---
+++ src/main/scala/edu/knoldus/Contants.scala
@@ -0,0 +1,8 @@
+package edu.knoldus
+
+object Constants {
+  val ONE = 1
+  val SIX = 6
+  val BLOGCOUNT = 5
+  val FIFTYONE = 51
+}
--- src/main/scala/edu/knoldus/Operation.scala
+++ src/main/scala/edu/knoldus/Operation.scala
@@ -15,58 +15,54 @@ class Operation {
   def favoriteBlog(blogList: Map[String, Int]): String = {
 
     blogList.maxBy(_._2).toString()
+
   }
 
-  val blogCount = 5
+  def blogCountIncrease(map: Map[String, Int], blogCount: Int = Constants.BLOGCOUNT): String = {
 
-  def blogCountIncrease(map: Map[String, Int], count: Int = blogCount): String = {
-
-
-    if (count > 0) {
+    if (blogCount > 0) {
 
       val keyArray = map.keySet.toArray
       val key = keyArray(Random.nextInt(keyArray.length))
       val value = map.getOrElse(key, 0)
       val tempMap = map + (key -> (value + 1))
-      blogCountIncrease(tempMap, count - 1)
+      blogCountIncrease(tempMap, blogCount - 1)
 
     }
+
     else {
 
       favoriteBlog(map)
 
     }
-
   }
 
   def diceRollandTrainer(person: Person): String = {
 
-    val trainerCount = 51
 
     def diceResult(name: String, count: Int = 3): String = {
 
       if (count > 0) {
-        val six = 6
-        val dice = Random.nextInt(six) + 1
+
+        val dice = Random.nextInt(Constants.SIX) + 1
         dice match {
-          case 1 | 6 => diceResult(name, count - 1)
+          case Constants.ONE | Constants.SIX => diceResult(name, count - 1)
           case _ => s"$name you are loser "
         }
       }
       else {
         s"$name you are winner"
       }
-
     }
 
     person match {
 
       case Gamer(name) => diceResult(name)
-      case Trainer(_) => Random.nextInt(trainerCount).toString
+      case Trainer(_) => Random.nextInt(Constants.FIFTYONE).toString
       case Blogger(_) => val map = Map("scala" -> 2, "java" -> 6, "kafka" -> 4)
         blogCountIncrease(map)
       case _ => "invalid person..."
-    }
 
+    }
   }
 }
