0ecfa2969a2b71159e364db78119c4b60a704b80
randhir <randhiricadbg@gmail.com>
2018-01-17 07:34:34 +0530

    solved without optimized( ques 3)

--- src/main/scala/edu/knoldus/Application.scala
+++ src/main/scala/edu/knoldus/Application.scala
@@ -6,20 +6,23 @@ object Application extends App {
 
   val log = Logger.getLogger(this.getClass)
 
-   val payment = new Payment
-   val amount = 100
+  val payment = new Payment
+  val amount = 100
 
-   log.info(payment.paymentBill(amount, "paytm") + "\n")
-   log.info(payment.paymentBill(amount, "freecharge") + "\n")
-   log.info(payment.paymentBill(amount, "net banking") + "\n")
-   log.info(payment.paymentBill(amount, "card payment") + "\n")
-   log.info(payment.paymentBill(amount, "cash") + "\n")
+  log.info(payment.paymentBill(amount, "paytm") + "\n")
+  log.info(payment.paymentBill(amount, "freecharge") + "\n")
+  log.info(payment.paymentBill(amount, "net banking") + "\n")
+  log.info(payment.paymentBill(amount, "card payment") + "\n")
+  log.info(payment.paymentBill(amount, "cash") + "\n")
 
   val operation = new Operation
 
   val gamer = Gamer("randhir")
   val trainer = Trainer("sangeetika")
+  val blogger = Blogger("prabhat")
+
   log.info(operation.diceRollandTrainer(gamer) + "\n")
   log.info(operation.diceRollandTrainer(trainer) + "\n")
+  log.info(operation.diceRollandTrainer(blogger) + "\n")
 
 }
--- src/main/scala/edu/knoldus/Operation.scala
+++ src/main/scala/edu/knoldus/Operation.scala
@@ -8,13 +8,40 @@ case class Gamer(name: String) extends Person
 
 case class Trainer(name: String) extends Person
 
+case class Blogger(name: String) extends Person
+
 class Operation {
+
+  def favoriteBlog(blogList: Map[String, Int]): String = {
+
+    blogList.maxBy(_._2).toString()
+  }
+
+  val blogCount = 5
+
+  def blogCountIncrease(map: Map[String, Int], count: Int = blogCount): String = {
+
+    val tempMap = collection.mutable.Map() ++ map
+    val keyArray = tempMap.keySet.toArray
+    val key = keyArray(Random.nextInt(keyArray.length))
+    if (count > 0) {
+      tempMap.update(key, tempMap(key) + 1)
+      val callMap = tempMap.toMap[String, Int]
+      blogCountIncrease(callMap, count - 1)
+    }
+    else {
+      val callMap = tempMap.toMap[String, Int]
+      favoriteBlog(callMap)
+    }
+
+  }
 
   def diceRollandTrainer(person: Person): String = {
 
     val trainerCount = 51
 
     def diceResult(name: String, count: Int = 3): String = {
+
       if (count > 0) {
         val six = 6
         val dice = Random.nextInt(six) + 1
@@ -26,12 +53,17 @@ class Operation {
       else {
         s"$name you are winner"
       }
+
     }
 
     person match {
+
       case Gamer(name) => diceResult(name)
       case Trainer(_) => Random.nextInt(trainerCount).toString
+      case Blogger(_) => val map = Map("scala" -> 2, "java" -> 6, "kafka" -> 4)
+        blogCountIncrease(map)
       case _ => "invalid person..."
     }
+
   }
 }
